ffb3128b3ddd142008337eef0e41e43f3919cf51
Jotchuar <joshteles@gmail.com>
2025-04-17 10:13:16 -0600

    adding canvas

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -3,55 +3,92 @@ const canvas = document.querySelector("#canvas");
 const saveBtn = document.querySelector("#saveBtn");
 const clearBtn = document.querySelector("#clearBtn");
 const colorPicker = document.querySelector("#colorPicker");
-
-// for dev
-const gridGen = document.querySelector("#gridGen");
+const saveCol = document.querySelector("#saveCol");
+const clearCol = document.querySelector("#clearCol");
+const swatches = document.querySelector("#swatches");
 
 let currentColor = colorPicker.value;
+let favoriteColors = {};
 
 // Event listeners
 clearBtn.addEventListener("click", clearGrid);
 colorPicker.addEventListener("input", (event) => changeColor(event));
-gridGen.addEventListener("click", renderGrid);
 
+saveCol.addEventListener("click", saveColor);
+clearCol.addEventListener("click", clearColors);
+
+// Save colors
+function saveColor() {
+    let objIndex = Object.keys(favoriteColors).length;
+    favoriteColors[objIndex] = currentColor;
+    console.log(favoriteColors);
+    console.log(objIndex);
+    createSwatch(objIndex);
+}
+
+// Clear saved colors
+function clearColors() {
+    favoriteColors = {};
+    for (keys in favoriteColors){
+        swatches.children.remove();
+    }
+}
+
+// Create an element for the color swatch
+function createSwatch(objIndex){
+    let hexVal = favoriteColors[objIndex];
+    let stringVal = String(hexVal);
+
+    let swatch = document.createElement("button");
+    swatch.id = `id-${stringVal.slice(1)}`;
+    swatch.style.backgroundColor = hexVal;
+    console.log(swatch.id);
+    swatches.appendChild(swatch);
+}
 
 // Color picker
-function changeColor(c){
+function changeColor(c) {
     currentColor = c.target.value;
     console.log(`Color changed to: ${c}`);
 }
 
 // Painting Pixels
-function paint(p){ 
-
+function paint(p) {
+    p.style.backgroundColor = currentColor;
+    console.log(`Painted pixel: ${p.id}`);
 }
 
 // Render grid
-function renderGrid(){
-    for (let i = 0; i < 16; i++){
+function renderGrid() {
+    for (let i = 0; i < 256; i++){
         let tempPix = makePixel(i);
-        console.log(tempPix);
-        document.getElementById("canvas").appendChild(tempPix);
+        // console.log(tempPix);
+        canvas.appendChild(tempPix);
     }
 }
 
-function makePixel(i){
+// create button elements (pixels)
+function makePixel(i) {
     let pixel = document.createElement("button");
-    pixel.id = `p${i}`;
-    pixel.style.height = "40px";
-    pixel.style.width = "40px";
+    pixel.id = `p${i+1}`;
+    pixel.style.height = "35px";
+    pixel.style.width = "35px";
     pixel.style.backgroundColor = "#FFFFFF";
-    pixel.style.margin = "0px";
-    pixel.style.padding = "0px";
-    pixel.addEventListener("click", paint(pixel));
+    pixel.style.padding = "0";
+    pixel.style.lineHeight = "0";
+    pixel.style.border = "1px solid rgba(0, 0, 0, 0.5)";
+    pixel.addEventListener("click", () => paint(pixel));
     return pixel;
 }
 
 // Clear grid
 function clearGrid(){
-    // add a prompt to ask the user if they are sure they want to clear
-    // to prevent accidental clearing
-    // "Are you sure you want to clear the canvas? This action cannot be undone."
+    let askToClear = confirm("Are you sure you want to clear the canvas? This action cannot be undone.");
+    if (askToClear) {
+        canvas.innerHTML = "";
+        console.clear();
+        renderGrid();
+    }
 }
 
 // Localstorage
